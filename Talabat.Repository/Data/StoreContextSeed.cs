4c0b4035c1962ab29667b7063db29cbf00784235
Ayman-Nashat <Ayman.nashat41@gmail.com>
2025-03-02 01:48:03 +0200

    Add project files.

---
+++ Talabat.Repository/Data/StoreContextSeed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Talabat.Repository.Data
+{
+    public static class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext dbContext)
+        {
+            await SeedDataAsync<ProductBrand>(dbContext, "brands.json");
+            await SeedDataAsync<ProductCategory>(dbContext, "categories.json");
+            await SeedDataAsync<Product>(dbContext, "products.json");
+            await SeedDataAsync<DeliveryMethod>(dbContext, "delivery.json");
+        }
+
+        private static async Task SeedDataAsync<T>(StoreContext dbContext, string fileName) where T : class
+        {
+            if (await dbContext.Set<T>().AnyAsync()) return;
+
+            var filePath = Path.Combine("../Talabat.Repository/Data/DataSeed", fileName);
+            if (!File.Exists(filePath)) return;
+
+            var jsonData = await File.ReadAllTextAsync(filePath);
+            var entities = JsonSerializer.Deserialize<List<T>>(jsonData);
+
+            if (entities is { Count: > 0 })
+            {
+                await dbContext.Set<T>().AddRangeAsync(entities);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Talabat.Repository/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.Repository.Identity
+{
+    public static class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> _userManager)
+        {
+            if(_userManager.Users.Count() == 0)
+            {
+                var user = new AppUser()
+                {
+                    DisplayName = "Ayman Nashat",
+                    Email = "ayman.nashat41@gmail.com",
+                    UserName = "ayman.nashat",
+                    PhoneNumber = "01152287677"
+                    
+                };
+
+                await _userManager.CreateAsync(user ,"Test@123");
+            }
+        }
+    }
+}
